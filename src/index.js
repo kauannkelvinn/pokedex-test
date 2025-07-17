c583b27c3938be00d5229cd9980c4d2e24e9a143
unknown <kauankelvin7777@gmail.com>
2025-07-17 15:48:43 -0300

    ajustado rotas

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { ThemeContextProvider } from './contexts/ThemeContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/pokedex-test">
+    <BrowserRouter>
       <ThemeContextProvider>
         <App />
       </ThemeContextProvider>
